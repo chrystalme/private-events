2b0c5b0320f28b9e3aeb6f2069cc17abcfa87bc1
Ahmet Bozaci <ahmt9417@gmail.com>
2021-03-02 15:35:13 +0300

    Create event model

---
+++ db/migrate/20210302123422_create_events.rb
@@ -0,0 +1,10 @@
+class CreateEvents < ActiveRecord::Migration[6.1]
+  def change
+    create_table :events do |t|
+      t.string :description
+      t.datetime :even_date
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_080855) do
+ActiveRecord::Schema.define(version: 2021_03_02_123422) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "description"
+    t.datetime "even_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
