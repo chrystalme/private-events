977253ab25fd0f6e4d3aae1f93a3b9fd3e16b5a8
Ahmet Bozaci <ahmt9417@gmail.com>
2021-03-04 12:59:17 +0300

    Create  user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  # get 'users/new'
+  # get 'users/show'
+  resources :users, only: %i[new create show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
