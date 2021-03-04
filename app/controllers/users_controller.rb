8884424efbe241faceb0271445e2c2668d73a77b
Ahmet Bozaci <ahmt9417@gmail.com>
2021-03-04 13:13:37 +0300

    Add create method in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,29 @@
 class UsersController < ApplicationController
+
   def new
+    @user = User.new
   end
 
-  def show
+  def create 
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "#{@user.name} created Successfully."
+    else
+      render :new
+    end
   end
+
+  # def show
+  #   if session[:user_id]
+  #     @user = User.find_by(id: session[:user_id])
+  #   end
+  # end
+
+  private
   
-  def create
+  def user_params
+    params.require(:user).permit(:name)
   end
 end
