3df1d46e812ef3ddf1fb4a393bdc04e585304c9c
chrystalme <afam_ifeanyi@live.com>
2021-03-04 16:14:52 +0200

    Add Event model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_094758) do
+ActiveRecord::Schema.define(version: 2021_03_04_135936) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "location"
+    t.datetime "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "creator_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
