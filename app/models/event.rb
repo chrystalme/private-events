b7e7cc98c18760743d5889940babc282bca1a857
Ahmet Bozaci <ahmt9417@gmail.com>
2021-03-05 13:46:59 +0300

    Create event ASSOCIATIONS

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
+  has_many :event_attendances, through: attended_events
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
  has_many :created_events, class_name: 'Event', foreign_key: 'creator_id'
+ has_many :event_attendances, foreign_key: 'attendee_id'
+ has_many :attended_events, through: 'event_attendances', source: 'Event'
 end
