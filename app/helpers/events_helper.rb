62c2c1463b698d8984d2fcb6fa0e62ac77fd796c
chrystalme <afam_ifeanyi@live.com>
2021-03-07 22:21:53 +0200

    Fix linters errors

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,14 +1,13 @@
 module EventsHelper
   def show_status(current_user)
     if logged_in?
-        if @created_event.creator == current_user 
-          "<span>Hosting  <i class='fa fa-check'></i></span>".html_safe
-        elsif @created_event.attendees.include?(current_user)
-          "<span>Attending  <i class='fa fa-check'></i></span>".html_safe
-        else
-          "#{button_to "Attend", attend_event_path, method: :get}".html_safe
-        end
-    else
+      if @created_event.creator == current_user
+        "<span>Hosting  <i class='fa fa-check'></i></span>".html_safe
+      elsif @created_event.attendees.include?(current_user)
+        "<span>Attending  <i class='fa fa-check'></i></span>".html_safe
+      else
+        "#{button_to 'Attend', attend_event_path, method: :get}".html_safe
+      end
     end
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,13 +4,13 @@ module UsersHelper
 
   def show_user_create(current_user)
     if logged_in?
-      if current_user.created_events.any?       
+      if current_user.created_events.any?   
         "<p> You have: #{current_user.created_events.count}  events </p>
-          #{link_to 'Create Event', new_event_path, class: 'button is-light'}".html_safe               
+          #{link_to 'Create Event', new_event_path, class: 'button is-light'}".html_safe   
       else
-      "<h1>No events </h1>
+        "<h1>No events </h1>
        #{link_to 'Create Event', new_event_path, class: 'button is-light'} ".html_safe
-      end     
+      end
     end
   end
 end
