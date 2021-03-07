f0c7a5672bf64a3f1471bebbbb9b0a8d4eb80df6
chrystalme <afam_ifeanyi@live.com>
2021-03-07 21:31:28 +0200

    Add past_events and upcoming_events to events_controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,6 +3,8 @@ class EventsController < ApplicationController
   before_action :logged_in?, except: [:index]
   def new
     @created_event = current_user.created_events.build
+    @past_events = Event.past_events
+    @upcoming_events = Event.upcoming_events
   end
 
   def index
