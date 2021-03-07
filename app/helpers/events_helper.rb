7c72f3377b57bc5c8646eab1f16aa40fb9a8b7d2
chrystalme <afam_ifeanyi@live.com>
2021-03-07 21:12:39 +0200

    Remove logic from view

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,14 @@
 module EventsHelper
+  def show_status(current_user)
+    if logged_in?
+        if @created_event.creator == current_user 
+          "<span>Hosting  <i class='fa fa-check'></i></span>".html_safe
+        elsif @created_event.attendees.include?(current_user)
+          "<span>Attending  <i class='fa fa-check'></i></span>".html_safe
+        else
+          "#{button_to "Attend", attend_event_path, method: :get}".html_safe
+        end
+    else
+    end
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,16 @@
 module UsersHelper
   @upcoming_events = []
   @past_events = []
+
+  def show_user_create(current_user)
+    if logged_in?
+      if current_user.created_events.any?       
+        "<p> You have: #{current_user.created_events.count}  events </p>
+          #{link_to 'Create Event', new_event_path, class: 'button is-light'}".html_safe               
+      else
+      "<h1>No events </h1>
+       #{link_to 'Create Event', new_event_path, class: 'button is-light'} ".html_safe
+      end     
+    end
+  end
 end
