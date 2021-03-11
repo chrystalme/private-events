798ff881b6aae7bf35da3b47cc7e66b4ec213e42
chrystalme <afam_ifeanyi@live.com>
2021-03-11 14:51:37 +0200

    Add index test for event controller

--- spec/controller/event_controller_spec.rb
+++ spec/controller/event_controller_spec.rb
@@ -1,3 +1,12 @@
 require 'rails_helper'
-require 'capybara'
+
+RSpec.describe 'event controller', type: :system do
+  describe 'index page' do
+    it 'shows the right content' do
+      visit events_path
+      sleep(5)
+      expect(page).to have_content('Private Events')
+    end
+  end
+end
 
